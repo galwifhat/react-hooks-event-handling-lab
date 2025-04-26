2be5768aa3b6ad217cb050f24ec4f1d3767d003a
Ruth <cirurue@gmail.com>
2025-04-26 12:11:57 +0300

    done

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,11 +1,12 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    const handleChange =(e)=>{
+        console.log("Entering password...");
+    }
     return (
       <div>
-        <input type="password" onChange={(e)=>{e.target.value
-            console.log("Entering password...")
-         }} />
+        <input type="password" onChange={handleChange} />
       </div>
     );
 }
